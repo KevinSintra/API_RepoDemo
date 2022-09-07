3871ee1a75ce21c5a3f96bfe626866cdba160f8e
kevin <kevin.fh.chan@cymmetrik.com>
2022-09-07 10:35:38 +0800

    Initial Commit

---
+++ API_RepoDemo/API_RepoDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_RepoDemoApp.swift
+//  API_RepoDemo
+//
+//  Created by CymmetrikDev2 on 2022/9/7.
+//
+
+import SwiftUI
+
+@main
+struct API_RepoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ API_RepoDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  API_RepoDemo
+//
+//  Created by CymmetrikDev2 on 2022/9/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
